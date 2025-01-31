edc0055e19a539a1e934501bc99f3310af38b407
Christopher Rateng <crateng13@gmail.com>
2025-01-31 10:50:22 +0300

    Initial commit: Monorepo structure with frontend and backend

---
+++ vibraafrika-frontend/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#85FFC7',
+        secondary: '#297373',
+        accent: '#FF8552',
+        light: '#E6E6E6',
+        dark: '#39393A',
+      },
+      fontFamily: {
+        rokkit: ['Rokkit', 'serif'],
+        brolimo: ['Brolimo', 'sans-serif'],
+        linden: ['Linden Hill', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+};
+
